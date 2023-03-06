b9f1b50abaca73146284d45b305f7029760a6019
RoninsWork <yurkaronin@gmail.com>
2023-03-06 19:39:12 +0300

    Начал писать мобильные элементы и скрипты

--- js/parts/catalog-button.js
+++ js/parts/catalog-button.js
@@ -1,12 +1,69 @@
-// кнопка каталога
-  // Find the element with the class .catalog-btn-call
-  const catalogBtn = document.getElementById('catalog-btn-call');
+// кнопка каталога десктоп
+const catalogBtn = document.getElementById('catalog-btn-call');
 
-  // Track the click event
-  catalogBtn.addEventListener('click', () => {
-    // Add or remove the .open css class on the clicked element
-    catalogBtn.classList.toggle('open');
+catalogBtn.addEventListener('click', () => {
+  catalogBtn.classList.toggle('open');
+  document.body.classList.toggle('catalog-menu-open');
+});
 
-    // Add or delete the .catalog-menu-open class to the body tag
-    document.body.classList.toggle('catalog-menu-open');
+// кнопка каталога мобилка
+const catalogBtnMob = document.getElementById('mob-catalog-btn-call');
+
+catalogBtnMob.addEventListener('click', () => {
+  catalogBtnMob.classList.toggle('open');
+  document.body.classList.toggle('catalog-menu-open');
+});
+
+// // кнопка меню
+// const menuBtn = document.getElementById('menu-button');
+
+// menuBtn.addEventListener('click', () => {
+//   menuBtn.classList.toggle('open');
+//   document.body.classList.toggle('mobile-menu-open');
+// });
+
+// // кнопка поиска
+// const searchBtn = document.getElementById('search-button');
+
+// searchBtn.addEventListener('click', () => {
+//   searchBtn.classList.toggle('open');
+//   document.body.classList.toggle('mobile-search-open');
+// });
+
+// menu button
+const menuBtn = document.getElementById('menu-button');
+// search button
+const searchBtn = document.getElementById('search-button');
+
+menuBtn.addEventListener('click', () => {
+  // toggle menu button class
+  menuBtn.classList.toggle('open');
+  // remove search button class if it exists
+  searchBtn.classList.remove('open');
+  document.body.classList.remove('mobile-search-open');
+  // toggle body class for mobile menu
+  document.body.classList.toggle('mobile-menu-open');
+});
+
+searchBtn.addEventListener('click', () => {
+  // toggle search button class
+  searchBtn.classList.toggle('open');
+  // remove menu button class if it exists
+  menuBtn.classList.remove('open');
+  document.body.classList.remove('mobile-menu-open');
+  // toggle body class for mobile search
+  document.body.classList.toggle('mobile-search-open');
+});
+
+
+
+
+// кнопки в мобильном меню
+// кнопка поиска
+const mobileSubMenuButtons = document.querySelectorAll('.mobile-menu__item--mod .button');
+for (let item of mobileSubMenuButtons) {
+  item.addEventListener('click', () => {
+    item.classList.toggle('open');
   });
+}
+
