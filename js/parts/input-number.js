69222ad4477e35f954e03a70e7f3093e3edaceeb
RoninsWork <yurkaronin@gmail.com>
2023-03-28 16:08:32 +0300

    +

--- js/parts/input-number.js
+++ js/parts/input-number.js
@@ -1,16 +1,19 @@
 
-var inputNumber = document.querySelector(".input-number");
-var minusBtn = inputNumber.querySelector(".input-number__button--minus");
-var plusBtn = inputNumber.querySelector(".input-number__button--plus");
-var input = inputNumber.querySelector(".input-number__input");
+var inputNumberList = document.querySelectorAll(".input-number");
 
-minusBtn.addEventListener("click", function () {
-  var currentValue = parseInt(input.value, 10) || 0;
-  input.value = currentValue - 1;
-});
+inputNumberList.forEach(function (inputNumber) {
+  var minusBtn = inputNumber.querySelector(".input-number__button--minus");
+  var plusBtn = inputNumber.querySelector(".input-number__button--plus");
+  var input = inputNumber.querySelector(".input-number__input");
 
-plusBtn.addEventListener("click", function () {
-  var currentValue = parseInt(input.value, 10) || 0;
-  input.value = currentValue + 1;
+  minusBtn.addEventListener("click", function () {
+    var currentValue = parseInt(input.value, 10) || 0;
+    input.value = currentValue - 1;
+  });
+
+  plusBtn.addEventListener("click", function () {
+    var currentValue = parseInt(input.value, 10) || 0;
+    input.value = currentValue + 1;
+  });
 });
 
