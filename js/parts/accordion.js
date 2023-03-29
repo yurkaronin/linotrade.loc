7a04d04270478de15797e893aff2db0e5969f51f
RoninsWork <yurkaronin@gmail.com>
2023-03-29 17:23:40 +0300

    Доработал скрипт аккордеонов

--- js/parts/accordion.js
+++ js/parts/accordion.js
@@ -1,6 +1,7 @@
 const accordions = document.querySelector('.accordions');
 if (document.querySelector('.accordions')) {
   const faqItems = document.querySelectorAll('.accordion');
+  const accordionHeaders = document.querySelectorAll('.accordion__question'); // Добавить эту строку
 
   // Expand the first accordion by default
   faqItems[0].classList.add('active');
@@ -8,19 +9,24 @@ if (document.querySelector('.accordions')) {
   accordions.addEventListener('click', (event) => {
     const item = event.target.closest('.accordion');
 
-    // Collapse all other accordions
-    faqItems.forEach(faqItem => {
-      if (faqItem !== item && faqItem.classList.contains('active')) {
-        faqItem.classList.remove('active');
-      }
-    });
+    // Добавить проверку на то, что клик произошел на заголовке аккордеона
+    const isHeaderClicked = [...accordionHeaders].some(header => header.contains(event.target));
 
-    // Expand/collapse the clicked accordion
-    if (item.classList.contains('active')) {
-      item.classList.remove('active');
-    } else {
-      item.classList.add('active');
+    // Если клик произошел на заголовке, то обрабатываем событие
+    if (isHeaderClicked) {
+      // Collapse all other accordions
+      faqItems.forEach(faqItem => {
+        if (faqItem !== item && faqItem.classList.contains('active')) {
+          faqItem.classList.remove('active');
+        }
+      });
+
+      // Expand/collapse the clicked accordion
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      } else {
+        item.classList.add('active');
+      }
     }
   });
 };
-
