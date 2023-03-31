d189de7f3f89f9208aa6863cf2a4cd01d268ed64
RoninsWork <yurkaronin@gmail.com>
2023-03-31 11:46:18 +0300

    Правки

--- js/parts/product-card-options.js
+++ js/parts/product-card-options.js
@@ -1,8 +1,17 @@
 const productCardOptions = document.querySelectorAll('.product-card__options');
+
 if (productCardOptions) {
   productCardOptions.forEach(option => {
     option.addEventListener('click', () => {
+      // Закрыть все остальные элементы перед открытием выбранного
+      productCardOptions.forEach(otherOption => {
+        if (otherOption !== option) {
+          otherOption.classList.remove('show');
+        }
+      });
+
+      // Открыть или закрыть выбранный элемент
       option.classList.toggle('show');
     });
   });
-};
+}
