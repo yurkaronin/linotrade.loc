8ae3906627764b8f5d2a7a8e821a018a7083fd45
RoninsWork <yurkaronin@gmail.com>
2023-04-05 15:48:48 +0300

    Доработка деталей в карточке анонса

--- js/parts/product-card-options.js
+++ js/parts/product-card-options.js
@@ -15,3 +15,28 @@ if (productCardOptions) {
     });
   });
 }
+
+let productCards = document.querySelectorAll('.product-card');
+let productCardsWithCart = [];
+
+productCards.forEach(function (card) {
+  if (card.querySelector('.product-card__cart')) {
+    productCardsWithCart.push(card);
+  }
+});
+
+productCardsWithCart.forEach(function (card) {
+  let btn = card.querySelector('.product-card__cart');
+  let details = card.querySelector('.product-card__details');
+
+  btn.addEventListener('click', function (event) {
+    event.preventDefault();
+    details.classList.toggle('show');
+  });
+
+  document.addEventListener('click', function (event) {
+    if (!card.contains(event.target)) {
+      details.classList.remove('show');
+    }
+  });
+});
